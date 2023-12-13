3b8a9063b6ca0fa404e76ec2ab6e882cf5358361
Matias Karjalainen <t1kama00@students.oamk.fi>
2023-12-13 13:19:57 +0200

    ylimääräisten kommenttien tmv poistelua

--- TL_Project_Week6/src/main.c
+++ TL_Project_Week6/src/main.c
@@ -134,8 +134,6 @@ void main(void)
 
 	while (1) 
 	{
-		//struct Measurement m = readADCValue();
-		//printk("x = %d,  y = %d,  z = %d\n",m.x,m.y,m.z);
 		
 		k_sleep(K_MSEC(1000));
 		
@@ -143,9 +141,6 @@ void main(void)
 		dk_set_led_on(USER_LED2);
 		dk_set_led_on(USER_LED3);
 		dk_set_led_on(USER_LED4);
-		//testiprinttejä, voi poistaa 
-		//printk("%f\n",calculateDistance(1,1,1,2,2,2));
-		//printk("%d\n",calculateDistanceToAllCentrePointsAndSelectWinner(1,1,1));
 		 
 		k_sleep(K_MSEC(1000));
 		
